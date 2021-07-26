be61061100aa85c0334dce5215823097f43f8634
Samarth Hattarwat <81406218+Samarthuh@users.noreply.github.com>
2021-07-26 22:26:24 +0530

    Add files via upload

---
+++ Project01.py
@@ -0,0 +1,41 @@
+# import required modules
+import requests, json
+
+# Enter your API key here
+api_key = "Your_API_Key"
+
+# base_url variable to store url
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+# Give city name
+city_name = input("Enter city name : ")
+
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+response = requests.get(complete_url)
+
+x = response.json()
+
+if x["cod"] != "404":
+
+	y = x["main"]
+
+	current_temperature = y["temp"]
+
+	current_pressure = y["pressure"]
+
+	current_humidity = y["humidity"]
+
+	z = x["weather"]
+
+	weather_description = z[0]["description"]
+
+	# print following values
+	print(" Temperature (in kelvin unit) = " +
+					str(current_temperature) +
+		"\n atmospheric pressure (in hPa unit) = " +
+					str(current_pressure) +
+		"\n humidity (in percentage) = " +
+					str(current_humidity) +
+		"\n description = " +
+					str(weather_description))
